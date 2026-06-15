3ebb872689d6936267fa1050ebb72c73cd6d5836
yindonglin0720 <yindonglin0720@gmail.com>
2026-06-15 19:31:11 +0800

    Day7: 周复盘 + 清理 test.py

--- scripts/test.py
+++
@@ -1,5 +0,0 @@
-import torch
-from torchvision import datasets, transforms
-
-transform=transforms.Compose([transforms.ToTensor(),transforms.Normalize((0.5,),(0.5,))])
-train_data=datasets.MNIST(root='./data',train=True,transform=transform,download=True)
